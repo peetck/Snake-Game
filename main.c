ccfa7cc899eb8a02446f2752e3a74ad874c7727a
peetck <peekung602@gmail.com>
2019-03-22 20:36:13 +0700

    Add MENU :+1:

--- main.c
+++ main.c
@@ -31,6 +31,12 @@ int main(int argc, char* args[]){
     bg_surface = IMG_Load("bg.png");
     bg_texture = SDL_CreateTextureFromSurface(renderer, bg_surface);
     SDL_Rect bg = {0, 0, 1200, 800};
+
+    SDL_Surface* menu_surface = NULL;
+    SDL_Texture* menu_texture = NULL;
+    menu_surface = IMG_Load("menu.png");
+    menu_texture = SDL_CreateTextureFromSurface(renderer, menu_surface);
+    SDL_Rect menu = {0, 0, 1200, 800};
     // โหลดตัวงู
     SDL_Surface* snake_surface = NULL;
     SDL_Texture* snake_texture = NULL;
@@ -61,6 +67,7 @@ int main(int argc, char* args[]){
     int keyboard_bug_fix = 1;
     int score = 0;
     char score_str[10000];
+    int menu_check = 1;
     // โปรแกรมรัน
     while (running){
         // เช็ค event
@@ -70,101 +77,111 @@ int main(int argc, char* args[]){
                 running = 0;
             }
             else if (event.type == SDL_KEYDOWN){
-                if (event.key.keysym.sym == SDLK_RIGHT && going != 'L' && keyboard_bug_fix){
+                if (event.key.keysym.sym == SDLK_RIGHT && going != 'L' && keyboard_bug_fix && menu_check == 2){
                     going = 'R';
                     keyboard_bug_fix = 0;
                 }
-                else if (event.key.keysym.sym == SDLK_LEFT && going != 'R' && keyboard_bug_fix){
+                else if (event.key.keysym.sym == SDLK_LEFT && going != 'R' && keyboard_bug_fix && menu_check == 2){
                     going = 'L';
                     keyboard_bug_fix = 0;
                 }
-                else if (event.key.keysym.sym == SDLK_UP && going != 'D' && keyboard_bug_fix){
+                else if (event.key.keysym.sym == SDLK_UP && going != 'D' && keyboard_bug_fix && menu_check == 2){
                     going = 'U';
                     keyboard_bug_fix = 0;
                 }
-                else if (event.key.keysym.sym == SDLK_DOWN && going != 'U' && keyboard_bug_fix){
+                else if (event.key.keysym.sym == SDLK_DOWN && going != 'U' && keyboard_bug_fix && menu_check == 2){
                     going = 'D';
                     keyboard_bug_fix = 0;
                 }
-            }
-        }
-        char mes[10000] = "Score: ";
-        sprintf(score_str, "%06d", score);
-        SDL_Surface* surfacemessage = TTF_RenderText_Solid(sans, strcat(mes, score_str), black);
-        SDL_Texture* message = SDL_CreateTextureFromSurface(renderer, surfacemessage);
-        SDL_RenderClear(renderer);
-        SDL_RenderCopy(renderer, bg_texture, NULL, &bg);
-        while (food_status){
-            food_texture = SDL_CreateTextureFromSurface(renderer, food_surface);
-            food_x = 20 * (rand() % 35);
-            food_y = 20 * (rand() % 35);
-            if (food_x == 0) food_x = 20;
-            if (food_y == 0) food_y = 20;
-            food_status = 0;
-            for (int i = 0; i < size; i++){
-                if (food_x == x[i] && food_y == y[i]){
-                    food_status++;
-                    break;
+                else if (menu_check == 1 && event.key.keysym.sym == SDLK_RETURN){
+                    menu_check++;
                 }
             }
         }
-        SDL_Rect food = {food_x, food_y, 20, 20};
-        SDL_RenderCopy(renderer, food_texture, NULL, &food);
-        for (int i = 0; i < size; i++){
-            SDL_Rect snake = {x[i], y[i], 20, 20};
-            SDL_RenderCopy(renderer, snake_texture, NULL, &snake);
+        if (menu_check == 1){
+            SDL_RenderClear(renderer);
+            SDL_RenderCopy(renderer, menu_texture, NULL, &menu);
+            SDL_RenderPresent(renderer);
         }
-        if (going == 'R'){
-            x[0] += 20;
-        }
-        else if (going == 'L'){
-            x[0] -= 20;
-        }
-        else if (going == 'U'){
-            y[0] -= 20;
-        }
-        else if (going == 'D'){
-            y[0] += 20;
-        }
-        if (x[0] > 760){
-            x[0] = 20;
-        }
-        if (x[0] < 20){
-            x[0] = 760;
-        }
-        if (y[0] > 760){
-            y[0] = 20;
-        }
-        if (y[0] < 20){
-            y[0] = 760;
-        }
-        if (x[0] == food_x && y[0] == food_y){
-            x[size] = x[size - 1] - 20;
-            size++;
-            food_status++;
-            SDL_DestroyTexture(food_texture);
-        }
-        for (int i = 1; i < size; i++){
-            if (x[0] == x[i] && y[0] == y[i]){
-                running = 0;
-                break;
+        else if (menu_check == 2){
+            char mes[10000] = "Score: ";
+            sprintf(score_str, "%06d", score);
+            SDL_Surface* surfacemessage = TTF_RenderText_Solid(sans, strcat(mes, score_str), black);
+            SDL_Texture* message = SDL_CreateTextureFromSurface(renderer, surfacemessage);
+            SDL_RenderClear(renderer);
+            SDL_RenderCopy(renderer, bg_texture, NULL, &bg);
+            while (food_status){
+                food_texture = SDL_CreateTextureFromSurface(renderer, food_surface);
+                food_x = 20 * (rand() % 35);
+                food_y = 20 * (rand() % 35);
+                if (food_x == 0) food_x = 20;
+                if (food_y == 0) food_y = 20;
+                food_status = 0;
+                for (int i = 0; i < size; i++){
+                    if (food_x == x[i] && food_y == y[i]){
+                        food_status++;
+                        break;
+                    }
+                }
             }
-            x[i] = temp_x[i - 1];
-            y[i] = temp_y[i - 1];
+            SDL_Rect food = {food_x, food_y, 20, 20};
+            SDL_RenderCopy(renderer, food_texture, NULL, &food);
+            for (int i = 0; i < size; i++){
+                SDL_Rect snake = {x[i], y[i], 20, 20};
+                SDL_RenderCopy(renderer, snake_texture, NULL, &snake);
+            }
+            if (going == 'R'){
+                x[0] += 20;
+            }
+            else if (going == 'L'){
+                x[0] -= 20;
+            }
+            else if (going == 'U'){
+                y[0] -= 20;
+            }
+            else if (going == 'D'){
+                y[0] += 20;
+            }
+            if (x[0] > 760){
+                x[0] = 20;
+            }
+            if (x[0] < 20){
+                x[0] = 760;
+            }
+            if (y[0] > 760){
+                y[0] = 20;
+            }
+            if (y[0] < 20){
+                y[0] = 760;
+            }
+            if (x[0] == food_x && y[0] == food_y){
+                x[size] = x[size - 1] - 20;
+                size++;
+                food_status++;
+                SDL_DestroyTexture(food_texture);
+            }
+            for (int i = 1; i < size; i++){
+                if (x[0] == x[i] && y[0] == y[i]){
+                    running = 0;
+                    break;
+                }
+                x[i] = temp_x[i - 1];
+                y[i] = temp_y[i - 1];
+            }
+            for (int i = 0; i < size; i++){
+                temp_x[i] = x[i];
+                temp_y[i] = y[i];
+            }
+            SDL_RenderCopy(renderer, message, NULL, &message_rect);
+            score++;
+            SDL_RenderPresent(renderer);
+            // ตั้งค่า delay time;
+            int delay= 3000 / 60 - SDL_GetTicks() + SDL_GetTicks();
+            if(delay > 0){
+                SDL_Delay(delay);
+            }
+            keyboard_bug_fix = 1;
         }
-        for (int i = 0; i < size; i++){
-            temp_x[i] = x[i];
-            temp_y[i] = y[i];
-        }
-        SDL_RenderCopy(renderer, message, NULL, &message_rect);
-        score++;
-        SDL_RenderPresent(renderer);
-        // ตั้งค่า delay time;
-        int delay= 3000 / 60 - SDL_GetTicks() + SDL_GetTicks();
-        if(delay > 0){
-            SDL_Delay(delay);
-        }
-        keyboard_bug_fix = 1;
     }
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
